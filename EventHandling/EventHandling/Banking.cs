aa456aeda2e07eaf47601349c21b8d291610c46d
Yashmunot <75272774+Yashmunot@users.noreply.github.com>
2020-12-04 17:16:19 +0530

    Add files via upload

---
+++ EventHandling/EventHandling/Banking.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHandling
+{
+    class Banking
+    {
+        
+
+        public int netBalance { get; set; }
+        
+        public void OverBalance(int amount)
+        {
+            double tax = amount * 0.18;
+            System.Windows.Forms.MessageBox.Show("You have to pay " + tax + " tax");
+        }
+        public void UnderBalance()
+        {
+            System.Windows.Forms.MessageBox.Show("Your balance is less than 5000, please maintain it more than 5000");
+        }
+        
+    }
+}
---
+++ EventHandling/EventHandling/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EventHandling
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Banking bng = new Banking();
+            bng.netBalance = Convert.ToInt32(textBox1.Text);
+            if (bng.netBalance >= 100000)
+            {
+                bng.OverBalance(bng.netBalance);
+            }
+            else if (bng.netBalance <= 5000)
+            {
+                bng.UnderBalance();
+            }
+            else
+            {
+                MessageBox.Show("No need to pay tax!!!");
+            }
+           
+            
+        }
+    }
+}
